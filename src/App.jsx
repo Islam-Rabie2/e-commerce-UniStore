707fc79284493e769d9458e7da34f5cfb59e7804
Islam-Rabie2 <islam.rabie25@gmail.com>
2024-07-01 10:27:14 -0800

    create more desgin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/pages/home/HomePage";
-import NoPage from "./components/pages/noPage/NoPage";
+import HomePage from "./pages/home/HomePage";
+import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from './components/scrollTop/ScrollTop';
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+
 function App() {
   return (
     <div>
       <Router>
+      <ScrollTop />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/*" element={<NoPage />} />
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/*" element={<NoPage/>} />
+          <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage/>}/>
+          <Route path="/allproduct" element={<AllProduct/>}/>
         </Routes>
       </Router>
     </div>
